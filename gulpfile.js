1a5efeac2f4590c777be3faee52106285de7c146
Phil Stavri <p.stavri@gmail.com>
2017-01-27 08:08:03 +0000

    Playing with Gulp and base tag for resource routing.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var pump = require("pump");
+var clean = require("gulp-clean");
+var runSequence = require("run-sequence");
 
 var vendorFiles = [
     "./node_modules/angular/angular.js",
@@ -7,14 +11,44 @@ var vendorFiles = [
     "./node_modules/angular-sanitize/angular-sanitize.js"
 ];
 
-gulp.task('build', function () {
-    console.log('Building...');
+var buildDir = "./build/";
 
-    return gulp.src(vendorFiles, { base: "./node_modules/" })
-        .pipe(concat("vendor2.js"))
-        .pipe(gulp.dest("./build/"));
+
+gulp.task("clean", function(){
+    return gulp.src(buildDir + "**/*")
+        .pipe(clean());
 });
 
-gulp.task('default', function () {
-    console.log('Hello, world!');
+gulp.task("html", function(){
+    return gulp.src("./src/index.html")
+        .pipe(gulp.dest(buildDir));
 });
+
+gulp.task("build", ["clean"], function(cb){
+    runSequence("html", "scripts:app", "scripts:vendor", cb);
+});
+
+gulp.task("scripts:app", function (cb) {
+    pump([
+            gulp.src("./src/**/*.js"),
+            concat("app.js"),
+            uglify(),
+            gulp.dest(buildDir)
+        ],
+        cb
+    );
+});
+
+gulp.task("scripts:vendor", function (cb) {
+
+    pump([
+            gulp.src(vendorFiles, { base: "./node_modules/" }),
+            concat("vendor.js"),
+            uglify(),
+            gulp.dest(buildDir)
+        ],
+        cb
+    );
+});
+
+gulp.task('default', ["build"]);
---
+++ src/app.js
@@ -0,0 +1,8 @@
+(function(){
+
+    var burgerApp = angular.module("burger-me", []);
+
+    burgerApp.run(function(){
+        console.log("burger run block");
+    });
+})();
